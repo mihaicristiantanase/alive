ae096e7bab4f1bfaf6e70d1b785a5c4a5f2002a5
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2022-04-08 09:40:38 +0300

    Create a macro for drawing pixels on screen and use it for drawing fractals

--- alive.lisp
+++ alive.lisp
@@ -172,18 +172,8 @@
           (cairo:fill-path))))))
 
 (defun draw-scene-algo-fractals ()
-  (cairo:set-source-rgb 0.2 0.2 0.2)
-  (cairo:paint)
-  (let* ((w (cairo:width cairo:*context*))
-         (h (cairo:height cairo:*context*))
-         (a (/ w h)))
-    (loop for px from 0 upto w by *2d-plot-point-size* do
-      (loop for py from 0 upto h by *2d-plot-point-size* do
-        (multiple-value-bind (r g b)
-            (mandelbrot px py w h a)
-          (cairo:set-source-rgb r g b)
-          (cairo:rectangle px py *2d-plot-point-size* *2d-plot-point-size*)
-          (cairo:fill-path))))))
+  (draw-scene-pixels
+   (mandelbrot px py w h a)))
 
 (defun update ()
   (format t "~&~a update~%" (local-time:now))
--- utils.lisp
+++ utils.lisp
@@ -4,3 +4,17 @@
   (+ (* (/ (- a in-min) (- in-max in-min))
         (- dst-max dst-min))
      dst-min))
+
+(defmacro draw-scene-pixels (&rest body)
+  `(progn
+     (cairo:set-source-rgb 0.2 0.2 0.2)
+     (cairo:paint)
+     (let* ((w (cairo:width cairo:*context*))
+            (h (cairo:height cairo:*context*))
+            (a (/ w h)))
+       (loop for px from 0 upto w by *2d-plot-point-size* do
+         (loop for py from 0 upto h by *2d-plot-point-size* do
+           (multiple-value-bind (r g b) ,@body
+             (cairo:set-source-rgb r g b)
+             (cairo:rectangle px py *2d-plot-point-size* *2d-plot-point-size*)
+             (cairo:fill-path)))))))
